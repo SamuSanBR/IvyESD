0e406c2377c8f58e5832809880aae6065c76586b
Samuel Cassan <samuel.cassan@ivytech.com>
2023-03-14 13:30:43 -0300

    database created; connection established;

--- Scan.cs
+++ Scan.cs
@@ -1,24 +1,29 @@
-using Microsoft.VisualBasic;
-using System.Text;
+using System.Data;
 using System.Text.RegularExpressions;
+using Microsoft.Data.SqlClient;
+
 
 namespace IvyESD
 {
     public partial class Scan : Form
     {
 
+        String str;
+        SqlConnection myConn = new SqlConnection("Server=VAL1WLAPGXK2XT2\\SQLEXPRESS;Integrated security=SSPI;database=IvyESD;Trust Server Certificate=true");
         public Scan()
         {
             InitializeComponent();
 
-
         }
+
 
         private void txtInputCode_KeyDown(object sender, KeyEventArgs e)
         {
+            //define o padrão de inserção para somente números
             string pattern = "^[0-9]+$";
             if (e.KeyCode == Keys.Enter)
-            {
+            {   
+                //checa se a entrada contém somente números
                 if (Regex.IsMatch(txtInputCode.Text, pattern) == true)
                 {
                     MessageBox.Show("Entrou");
@@ -26,6 +31,7 @@ namespace IvyESD
                     txtInputCode.Enabled = false;
                     txtCode.Text = txtInputCode.Text;
                     gbInfo.Visible = true;
+                    Connection();
                 }
                 else
                 {
@@ -34,6 +40,29 @@ namespace IvyESD
                 }
             }
 
+        }
+
+        private void Connection()
+        {
+            str = "SELECT * FROM dbo.USERS where Id = ";
+            SqlCommand myCommand = new SqlCommand(str, myConn);
+            try
+            {
+                myConn.Open();
+                myCommand.ExecuteNonQuery();
+                MessageBox.Show("Criou a conn", "IvyESD", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (System.Exception ex)
+            {
+                MessageBox.Show(ex.ToString(), "IvyESD", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            finally
+            {
+                if (myConn.State == ConnectionState.Open)
+                {
+                    myConn.Close();
+                }
+            }
         }
     }
 }
