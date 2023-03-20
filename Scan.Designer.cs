2ef68b15be06b5ee41765c56ae5909c9be1c89fb
samuel.cassan <samuel.cassan@hotmail.com>
2023-03-20 12:26:25 -0300

    test validation; reset screen; connection with db

--- Scan.Designer.cs
+++ Scan.Designer.cs
@@ -31,7 +31,7 @@
             this.gbScan = new System.Windows.Forms.GroupBox();
             this.txtInputCode = new System.Windows.Forms.TextBox();
             this.lblScan = new System.Windows.Forms.Label();
-            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radbtnCalcanheiraPulseira = new System.Windows.Forms.RadioButton();
             this.radbtnCalcanheira = new System.Windows.Forms.RadioButton();
             this.txtTest = new System.Windows.Forms.TextBox();
             this.lblExpectedTest = new System.Windows.Forms.Label();
@@ -51,20 +51,21 @@
             // 
             this.gbScan.Controls.Add(this.txtInputCode);
             this.gbScan.Controls.Add(this.lblScan);
-            this.gbScan.Location = new System.Drawing.Point(12, 9);
+            this.gbScan.Location = new System.Drawing.Point(26, 18);
             this.gbScan.Margin = new System.Windows.Forms.Padding(0);
             this.gbScan.Name = "gbScan";
             this.gbScan.Padding = new System.Windows.Forms.Padding(0);
-            this.gbScan.Size = new System.Drawing.Size(467, 111);
+            this.gbScan.Size = new System.Drawing.Size(992, 228);
             this.gbScan.TabIndex = 0;
             this.gbScan.TabStop = false;
             // 
             // txtInputCode
             // 
             this.txtInputCode.Font = new System.Drawing.Font("Arial Narrow", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.txtInputCode.Location = new System.Drawing.Point(8, 60);
+            this.txtInputCode.Location = new System.Drawing.Point(17, 123);
+            this.txtInputCode.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.txtInputCode.Name = "txtInputCode";
-            this.txtInputCode.Size = new System.Drawing.Size(446, 38);
+            this.txtInputCode.Size = new System.Drawing.Size(943, 69);
             this.txtInputCode.TabIndex = 1;
             this.txtInputCode.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtInputCode_KeyDown);
             // 
@@ -72,52 +73,59 @@
             // 
             this.lblScan.AutoSize = true;
             this.lblScan.Font = new System.Drawing.Font("Arial Narrow", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.lblScan.Location = new System.Drawing.Point(1, 22);
+            this.lblScan.Location = new System.Drawing.Point(2, 45);
+            this.lblScan.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.lblScan.Name = "lblScan";
-            this.lblScan.Size = new System.Drawing.Size(453, 33);
+            this.lblScan.Size = new System.Drawing.Size(906, 65);
             this.lblScan.TabIndex = 0;
             this.lblScan.Text = "Por favor, aproxime seu código do scanner.";
             // 
-            // radioButton1
+            // radbtnCalcanheiraPulseira
             // 
-            this.radioButton1.AutoSize = true;
-            this.radioButton1.Font = new System.Drawing.Font("Arial Narrow", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.radioButton1.Location = new System.Drawing.Point(14, 89);
-            this.radioButton1.Name = "radioButton1";
-            this.radioButton1.Size = new System.Drawing.Size(201, 31);
-            this.radioButton1.TabIndex = 6;
-            this.radioButton1.TabStop = true;
-            this.radioButton1.Text = "Calcanheira/Pulseira";
-            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radbtnCalcanheiraPulseira.AutoSize = true;
+            this.radbtnCalcanheiraPulseira.Font = new System.Drawing.Font("Arial Narrow", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.radbtnCalcanheiraPulseira.Location = new System.Drawing.Point(30, 182);
+            this.radbtnCalcanheiraPulseira.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.radbtnCalcanheiraPulseira.Name = "radbtnCalcanheiraPulseira";
+            this.radbtnCalcanheiraPulseira.Size = new System.Drawing.Size(408, 59);
+            this.radbtnCalcanheiraPulseira.TabIndex = 6;
+            this.radbtnCalcanheiraPulseira.TabStop = true;
+            this.radbtnCalcanheiraPulseira.Text = "Calcanheira/Pulseira";
+            this.radbtnCalcanheiraPulseira.UseVisualStyleBackColor = true;
+            this.radbtnCalcanheiraPulseira.Click += new System.EventHandler(this.radbtnCalcanheiraPulseira_Click);
             // 
             // radbtnCalcanheira
             // 
             this.radbtnCalcanheira.AutoSize = true;
             this.radbtnCalcanheira.Font = new System.Drawing.Font("Arial Narrow", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.radbtnCalcanheira.Location = new System.Drawing.Point(14, 52);
+            this.radbtnCalcanheira.Location = new System.Drawing.Point(30, 107);
+            this.radbtnCalcanheira.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.radbtnCalcanheira.Name = "radbtnCalcanheira";
-            this.radbtnCalcanheira.Size = new System.Drawing.Size(130, 31);
+            this.radbtnCalcanheira.Size = new System.Drawing.Size(262, 59);
             this.radbtnCalcanheira.TabIndex = 5;
             this.radbtnCalcanheira.TabStop = true;
             this.radbtnCalcanheira.Text = "Calcanheira";
             this.radbtnCalcanheira.UseVisualStyleBackColor = true;
+            this.radbtnCalcanheira.Click += new System.EventHandler(this.radbtnCalcanheira_Click);
             // 
             // txtTest
             // 
             this.txtTest.Enabled = false;
             this.txtTest.Font = new System.Drawing.Font("Arial Narrow", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.txtTest.Location = new System.Drawing.Point(15, 213);
+            this.txtTest.Location = new System.Drawing.Point(32, 437);
+            this.txtTest.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.txtTest.Name = "txtTest";
-            this.txtTest.Size = new System.Drawing.Size(362, 34);
+            this.txtTest.Size = new System.Drawing.Size(765, 60);
             this.txtTest.TabIndex = 11;
             // 
             // lblExpectedTest
             // 
             this.lblExpectedTest.AutoSize = true;
             this.lblExpectedTest.Font = new System.Drawing.Font("Arial Narrow", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.lblExpectedTest.Location = new System.Drawing.Point(8, 183);
+            this.lblExpectedTest.Location = new System.Drawing.Point(17, 375);
+            this.lblExpectedTest.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.lblExpectedTest.Name = "lblExpectedTest";
-            this.lblExpectedTest.Size = new System.Drawing.Size(150, 27);
+            this.lblExpectedTest.Size = new System.Drawing.Size(311, 55);
             this.lblExpectedTest.TabIndex = 10;
             this.lblExpectedTest.Text = "Teste necessário";
             // 
@@ -125,18 +133,20 @@
             // 
             this.txtName.Enabled = false;
             this.txtName.Font = new System.Drawing.Font("Arial Narrow", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.txtName.Location = new System.Drawing.Point(15, 126);
+            this.txtName.Location = new System.Drawing.Point(32, 258);
+            this.txtName.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.txtName.Name = "txtName";
-            this.txtName.Size = new System.Drawing.Size(362, 34);
+            this.txtName.Size = new System.Drawing.Size(765, 60);
             this.txtName.TabIndex = 9;
             // 
             // lblName
             // 
             this.lblName.AutoSize = true;
             this.lblName.Font = new System.Drawing.Font("Arial Narrow", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.lblName.Location = new System.Drawing.Point(8, 96);
+            this.lblName.Location = new System.Drawing.Point(17, 197);
+            this.lblName.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.lblName.Name = "lblName";
-            this.lblName.Size = new System.Drawing.Size(62, 27);
+            this.lblName.Size = new System.Drawing.Size(124, 55);
             this.lblName.TabIndex = 8;
             this.lblName.Text = "Nome";
             // 
@@ -144,12 +154,12 @@
             // 
             this.gbInfo.Controls.Add(this.lblSelect);
             this.gbInfo.Controls.Add(this.radbtnCalcanheira);
-            this.gbInfo.Controls.Add(this.radioButton1);
-            this.gbInfo.Location = new System.Drawing.Point(12, 135);
+            this.gbInfo.Controls.Add(this.radbtnCalcanheiraPulseira);
+            this.gbInfo.Location = new System.Drawing.Point(26, 277);
             this.gbInfo.Margin = new System.Windows.Forms.Padding(0);
             this.gbInfo.Name = "gbInfo";
             this.gbInfo.Padding = new System.Windows.Forms.Padding(0);
-            this.gbInfo.Size = new System.Drawing.Size(467, 138);
+            this.gbInfo.Size = new System.Drawing.Size(992, 283);
             this.gbInfo.TabIndex = 12;
             this.gbInfo.TabStop = false;
             this.gbInfo.Visible = false;
@@ -158,9 +168,10 @@
             // 
             this.lblSelect.AutoSize = true;
             this.lblSelect.Font = new System.Drawing.Font("Arial Narrow", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.lblSelect.Location = new System.Drawing.Point(8, 20);
+            this.lblSelect.Location = new System.Drawing.Point(17, 41);
+            this.lblSelect.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.lblSelect.Name = "lblSelect";
-            this.lblSelect.Size = new System.Drawing.Size(324, 33);
+            this.lblSelect.Size = new System.Drawing.Size(654, 65);
             this.lblSelect.TabIndex = 7;
             this.lblSelect.Text = "Quais testes foram realizados?";
             // 
@@ -172,11 +183,11 @@
             this.gbSelect.Controls.Add(this.txtTest);
             this.gbSelect.Controls.Add(this.lblName);
             this.gbSelect.Controls.Add(this.lblExpectedTest);
-            this.gbSelect.Location = new System.Drawing.Point(487, 9);
+            this.gbSelect.Location = new System.Drawing.Point(1035, 18);
             this.gbSelect.Margin = new System.Windows.Forms.Padding(0);
             this.gbSelect.Name = "gbSelect";
             this.gbSelect.Padding = new System.Windows.Forms.Padding(0);
-            this.gbSelect.Size = new System.Drawing.Size(402, 264);
+            this.gbSelect.Size = new System.Drawing.Size(854, 541);
             this.gbSelect.TabIndex = 13;
             this.gbSelect.TabStop = false;
             this.gbSelect.Visible = false;
@@ -185,30 +196,33 @@
             // 
             this.txtCode.Enabled = false;
             this.txtCode.Font = new System.Drawing.Font("Arial Narrow", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.txtCode.Location = new System.Drawing.Point(15, 48);
+            this.txtCode.Location = new System.Drawing.Point(32, 98);
+            this.txtCode.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.txtCode.Name = "txtCode";
-            this.txtCode.Size = new System.Drawing.Size(362, 34);
+            this.txtCode.Size = new System.Drawing.Size(765, 60);
             this.txtCode.TabIndex = 13;
             // 
             // lblCode
             // 
             this.lblCode.AutoSize = true;
             this.lblCode.Font = new System.Drawing.Font("Arial Narrow", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.lblCode.Location = new System.Drawing.Point(8, 18);
+            this.lblCode.Location = new System.Drawing.Point(17, 37);
+            this.lblCode.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.lblCode.Name = "lblCode";
-            this.lblCode.Size = new System.Drawing.Size(70, 27);
+            this.lblCode.Size = new System.Drawing.Size(143, 55);
             this.lblCode.TabIndex = 12;
             this.lblCode.Text = "Código";
             // 
             // Scan
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(17F, 41F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(896, 284);
+            this.ClientSize = new System.Drawing.Size(1904, 582);
             this.ControlBox = false;
             this.Controls.Add(this.gbSelect);
             this.Controls.Add(this.gbInfo);
             this.Controls.Add(this.gbScan);
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "Scan";
@@ -228,7 +242,7 @@
         private GroupBox gbScan;
         private TextBox txtInputCode;
         private Label lblScan;
-        private RadioButton radioButton1;
+        private RadioButton radbtnCalcanheiraPulseira;
         private RadioButton radbtnCalcanheira;
         private TextBox txtTest;
         private Label lblExpectedTest;
--- Scan.cs
+++ Scan.cs
@@ -7,7 +7,8 @@ namespace IvyESD
 {
     public partial class Scan : Form
     {
-
+        String Id, Name, Test;
+        SqlDataReader rdr;
         String str;
         SqlConnection myConn = new SqlConnection("Server=VAL1WLAPGXK2XT2\\SQLEXPRESS;Integrated security=SSPI;database=IvyESD;Trust Server Certificate=true");
         public Scan()
@@ -26,12 +27,13 @@ namespace IvyESD
                 //checa se a entrada contém somente números
                 if (Regex.IsMatch(txtInputCode.Text, pattern) == true)
                 {
-                    MessageBox.Show("Entrou");
                     gbSelect.Visible = true;
                     txtInputCode.Enabled = false;
                     txtCode.Text = txtInputCode.Text;
                     gbInfo.Visible = true;
                     Connection();
+                    LookUpForUserInDB();
+
                 }
                 else
                 {
@@ -43,26 +45,148 @@ namespace IvyESD
         }
 
         private void Connection()
-        {
-            str = "SELECT * FROM dbo.USERS where Id = ";
-            SqlCommand myCommand = new SqlCommand(str, myConn);
-            try
+        {       
+                try
             {
                 myConn.Open();
-                myCommand.ExecuteNonQuery();
-                MessageBox.Show("Criou a conn", "IvyESD", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                //MessageBox.Show("Connected", "IvyESD", MessageBoxButtons.OK, MessageBoxIcon.Information);
             }
             catch (System.Exception ex)
             {
                 MessageBox.Show(ex.ToString(), "IvyESD", MessageBoxButtons.OK, MessageBoxIcon.Information);
             }
-            finally
-            {
+        }
+
+        private void CloseConn()
+        {
                 if (myConn.State == ConnectionState.Open)
                 {
                     myConn.Close();
                 }
+                else
+                {
+                MessageBox.Show("Connection already closed", "IvyESD", MessageBoxButtons.OK, MessageBoxIcon.Information);
             }
         }
+
+        private void LookUpForUserInDB()
+        {
+            str = " SELECT Name, Test FROM dbo.USERS where Id = @inputcode ";
+            SqlCommand myCommand = new SqlCommand(str, myConn);
+            myCommand.Parameters.AddWithValue("@inputcode", txtInputCode.Text);
+
+            myCommand.ExecuteNonQuery();
+            rdr = myCommand.ExecuteReader();
+            while (rdr.Read())
+            {
+                Name = rdr["Name"].ToString();
+                Test = rdr["Test"].ToString();
+            }
+            Id = txtInputCode.Text;
+            txtName.Text = Name;
+            txtTest.Text = Test;
+            CloseConn();
+        }
+
+        private void InsertLogInDB()
+        {
+            str = "INSERT INTO dbo.LOG (UserId, Test_Date, Test) VALUES (@id, @date, @test)";
+            SqlCommand myCommand = new SqlCommand(str, myConn);
+            myCommand.Parameters.AddWithValue("@id", Id);
+            myCommand.Parameters.AddWithValue("@date", DateTime.Now.ToString("dd-MM-yyyy HH:mm:ss"));
+            myCommand.Parameters.AddWithValue("@test", Test);
+            myCommand.ExecuteNonQuery();
+            MessageBox.Show("Registro salvo com sucesso", "IvyESD", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+        private void radbtnCalcanheira_Click(object sender, EventArgs e)
+        {
+            if (Test.ToString() != "Calcanheira")
+            {
+                DialogResult GoingRogue = MessageBox.Show("O teste obrigatório para esse usuário é diferente do informado. Deseja continuar?", "IvyESD", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+                if(GoingRogue == DialogResult.Yes)
+                {
+                    MessageBox.Show("MODO F LIGADO", "ROGUE", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    radbtnCalcanheira.Enabled = false;
+                    radbtnCalcanheiraPulseira.Enabled = false;
+                    Test = "Calcanheira";
+                    Connection();
+                    InsertLogInDB();
+                    CloseConn();
+                    ResetScreen();
+                }
+                else
+                {
+                    MessageBox.Show("BOM MENINO", "PESSOA DE BEM", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    radbtnCalcanheira.Checked = false;
+                    radbtnCalcanheiraPulseira.Checked = false;
+                }
+            }
+            else
+            {
+                radbtnCalcanheira.Enabled = false;
+                radbtnCalcanheiraPulseira.Enabled = false;
+                Connection();
+                InsertLogInDB();
+                CloseConn();
+                ResetScreen();
+            }
+        }
+
+        private void radbtnCalcanheiraPulseira_Click(object sender, EventArgs e)
+        {
+            if (Test.ToString() != "Calcanheira/Pulseira")
+            {
+                DialogResult GoingRogue = MessageBox.Show("O teste obrigatório para esse usuário é diferente do informado. Deseja continuar?", "IvyESD", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+                if (GoingRogue == DialogResult.Yes)
+                {
+                    MessageBox.Show("MODO F LIGADO", "ROGUE", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    radbtnCalcanheiraPulseira.Enabled = false;
+                    radbtnCalcanheira.Enabled = false;
+                    radbtnCalcanheira.Enabled = false;
+                    radbtnCalcanheiraPulseira.Enabled = false;
+                    Test = "Calcanheira/Pulseira";
+                    Connection();
+                    InsertLogInDB();
+                    CloseConn();
+                    ResetScreen();
+                }
+                else
+                {
+                    MessageBox.Show("BOM MENINO", "PESSOA DE BEM", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    radbtnCalcanheira.Checked = false;
+                    radbtnCalcanheiraPulseira.Checked = false;
+                }
+            }
+            else
+            {
+                radbtnCalcanheira.Enabled = false;
+                radbtnCalcanheiraPulseira.Enabled = false;
+                Connection();
+                InsertLogInDB();
+                CloseConn();
+                ResetScreen();
+            }
+        }
+
+        private void ResetScreen()
+        {
+            txtInputCode.Text = "";
+            txtInputCode.Enabled = true;
+            txtInputCode.Focus();
+
+            radbtnCalcanheira.Checked = false;
+            radbtnCalcanheiraPulseira.Checked = false;
+            radbtnCalcanheira.Enabled = true;
+            radbtnCalcanheiraPulseira.Enabled = true;
+
+            txtCode.Text = "";
+            txtName.Text = "";
+            txtTest.Text = "";
+
+            gbInfo.Visible = false;
+            gbSelect.Visible = false;
+        }
+
+        
     }
 }
